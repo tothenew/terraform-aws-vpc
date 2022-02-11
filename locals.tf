4c388ec9051cf2fad7dc606cdc00367bec562974
Prashant Gupta <prashant.gupta@tothenew.com>
2022-02-11 13:59:51 +0530

    Completed full VPC through subnet wise

--- locals.tf
+++ locals.tf
@@ -6,15 +6,6 @@ locals {
         Environment = "dev"
     }
 
-    subnet_bits      = {for bit, bit_value in var.subnet : bit => bit_value.new_bits}
-    subnet_cidr_list = cidrsubnets(var.cidr_block, values(local.subnet_bits)...)
-    subnet_cidr_map  = {for i, cidr in local.subnet_cidr_list : keys(local.subnet_bits)[i] => cidr}
-    addrs_by_name    = {
-    for i, n in var.subnet : i => {
-        cidr_block        = local.subnet_cidr_map[i]
-        availability_zone = "${var.region}${n.availability_zone}"
-        is_private        = n.is_private
-    }
-    }
-    subnet_cidr_blocks = tomap(local.addrs_by_name)
+    public_subnet_name =  [for subnet_name, subnet_detail1 in var.subnet : subnet_name if subnet_detail1.is_public == true][0]
+
 }
--- output.tf
+++ output.tf
@@ -10,26 +10,14 @@ output "subnet_ids" {
   value = tomap({ for k, bd in module.subnet_main : k => bd.subnet_id })
 }
 
-output "network_cidr_blocks" {
-  value = local.subnet_cidr_blocks
-}
-
 output "internet_gateway_id" {
   value = module.internet_gateway.internet_gateway_id
 }
 
-output "public_route_table_id" {
-  value = module.public_route_table.route_table_id
-}
-
-output "private_route_table_id" {
-  value = module.private_route_table.route_table_id
+output "route_table_id" {
+    value = tomap({for k, bd in module.route_table : k => bd.route_table_id})
 }
 
 output "eip_id" {
   value = module.elastic_ip.eip_id
-}
-
-output "subnet_cidr" {
-  value = tomap({ for k, bd in local.subnet_cidr_blocks : k => lookup(bd, "cidr_block", "undefined") })
 }
