d1a2d334f5ec98bebffdf5fe109ded9ce80e58e8
Prashant Gupta <prashant.gupta@tothenew.com>
2023-02-08 10:50:37 +0530

    Added vpc example

---
+++ examples/simple-vpc-less-subnet/main.tf
@@ -0,0 +1,17 @@
+# Overriding Default configuration will create 2 private and 2 public VPC Subnets
+
+module "vpc_main" {
+  source      = "git::https://github.com/tothenew/terraform-aws-vpc.git?ref=v0.1.0"
+  cidr_block  = "10.1.0.0/16"
+  subnet_bits = 8
+  subnet_group = {
+    "private" = {
+      is_public   = false
+      nat_gateway = true
+    },
+    "public" = {
+      is_public   = true
+      nat_gateway = false
+    }
+  }
+}
--- examples/simple-vpc/main.tf
+++ examples/simple-vpc/main.tf
@@ -1,3 +1,5 @@
+# Default configuration will create 2 private, 2 public and 2 database VPC Subnets
+
 module "vpc_main" {
   source      = "git::https://github.com/tothenew/terraform-aws-vpc.git?ref=v0.1.0"
   cidr_block  = "10.1.0.0/16"
